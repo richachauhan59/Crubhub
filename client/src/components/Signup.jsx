deb824144af929cf40d12930ac7479da094e1c4f
Shreyas Shriyan <51715896+shreyas-shriyan@users.noreply.github.com>
2020-11-05 17:42:28 +0530

    created signup page

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -12,13 +12,23 @@ export default function Login() {
             <div className={styles.mainCard}>
                 <div style={{ padding: '10px 30px' }}>
                     <h2 className={styles.signInText}>
-                        Sign in with your Crubhub account
+                        Create your account
                     </h2>
                     <form style={{ display: 'flex', flexDirection: 'column' }}>
+                        <div style={{ display: "flex", justifyContent: "space-between" }}>
+                            <div>
+                                <div className={styles.placeholder}>First name</div>
+                                <input type="text" className={styles.halfInput}></input>
+                            </div>
+                            <div>
+                                <div className={styles.placeholder}>Last name</div>
+                                <input type="text" className={styles.halfInput}></input>
+                            </div>
+                        </div>
                         <div className={styles.placeholder}>Email</div>
                         <input type="email" className={styles.input}></input>
-                        <div className={styles.placeholder}>Password</div>
-                        <input type="password" className={styles.input}></input>
+                        <div className={styles.placeholder}>Password (8 character minimum)</div>
+                        <input type="password" minlength="8" className={styles.input}></input>
                         <div className={styles.utility}>
                             <div style={{ display: "flex", alignItems: "center" }}>
                                 <Checkbox
@@ -26,23 +36,12 @@ export default function Login() {
                                     style={{ color: "#6b6b83", marginLeft: "-11px" }}
                                     inputProps={{ 'aria-label': 'secondary checkbox' }}
                                 />
-
                                 <div style={{ fontSize: "15px", color: "#6b6b83" }}>Keep me signed in</div>
                             </div>
                             <div>
-                                <a
-                                    style={{
-                                        textDecoration: 'none',
-                                        color: '#0070eb',
-                                        fontSize: '15px'
-                                    }}
-                                    href="https://www.google.com"
-                                >
-                                    Reset password
-                                </a>
                             </div>
                         </div>
-                        <button className={styles.button}>Sign in</button>
+                        <button className={styles.createButton}>Create your account</button>
                     </form>
                     <div>or</div>
                     <FacebookLogin
@@ -64,13 +63,42 @@ export default function Login() {
                         buttonText="Login"
                         cookiePolicy={'single_host_origin'}
                     />
-                    <div style={{ marginTop: '15px', marginBottom: '35px' }}>
-                        <Link to="/signup" style={{
+                    <div style={{ marginTop: "10px", fontSize: "15px" }} >Have an account?
+                         <Link to="/login" style={{
                             textDecoration: 'none',
                             color: '#0070eb',
-                            fontSize: '15px',
-                            fontWeight: '500'
-                        }} >Create your account</Link>
+                            fontSize: '14px',
+                            fontWeight: '500',
+                            marginLeft: "10px"
+                        }} >Sign in</Link>
+                    </div>
+                    <div style={{ fontSize: "13px", marginTop: "10px", marginBottom: "40px", fontWeight: '450', }} >By creating your Grubhub account, you agree to the
+                        <a
+                            style={{
+                                textDecoration: 'none',
+                                color: '#0070eb',
+                                fontSize: '13px',
+                                fontWeight: '600',
+                                marginLeft: "10px",
+                                marginRight: "10px"
+                            }}
+                            href="https://www.google.com"
+                        >
+                            Terms of Use
+                        </a>
+                        and
+                        <a
+                            style={{
+                                textDecoration: 'none',
+                                color: '#0070eb',
+                                fontSize: '13px',
+                                fontWeight: '600',
+                                marginLeft: "10px"
+                            }}
+                            href="https://www.google.com"
+                        >
+                            Privacy Policy.
+                        </a>
                     </div>
                 </div>
             </div>
--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import Home from '../components/Home';
 import Login from '../components/Login';
+import Signup from '../components/Signup';
 
 export default function Routes() {
     return (
@@ -13,6 +14,7 @@ export default function Routes() {
                     render={(props) => <Home {...props}></Home>}
                 ></Route>
                 <Route path="/login" render={() => <Login></Login>}></Route>
+                <Route path="/signup" render={() => <Signup></Signup>}></Route>
             </Switch>
         </div>
     );
